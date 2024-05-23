daf8a6bc723ee795c71152ca704ee26a04ad45b6
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2024-05-23 11:42:19 +0100

    chore: update sheet UI

--- src/app/(root)/_components/create-rate-alert/create-rate-alert.tsx
+++ src/app/(root)/_components/create-rate-alert/create-rate-alert.tsx
@@ -7,12 +7,13 @@ import {
   SheetPortal,
   SheetTitle,
 } from '@/components/ui/sheet/sheet'
-import { ArrowUpDown, X } from 'lucide-react'
+import { ArrowUpDown, Scroll, X } from 'lucide-react'
 import { CurrencySelect } from '../currency-select/currency-select'
 import { Input } from '@/components/ui/input/input'
 import { CurrencyIcon } from '@/components/ui/currency-icon/currency-icon'
 import { Switch } from '@/components/ui/switch/switch'
 import { Label } from '@/components/ui/label/label'
+import { ScrollArea } from '@/components/ui/scroll-area/scroll-area'
 
 interface CreateRateAlertProps {
   open: boolean
@@ -35,13 +36,13 @@ export const CreateRateAlert = ({
       }}
     >
       <SheetPortal>
-        <SheetContent>
+        <SheetContent className="sm:max-w-0 sm:min-w-full">
           <SheetHeader className="mx-auto w-full max-w-3xl">
             <SheetTitle asChild>
               <div>
                 <div className="border-b mb-2 pb-2 text-left">
                   <h3 className="mb-1">Rate watch</h3>
-                  <p className="text-muted-foreground font-normal tracking-normal text-base">
+                  <p className="text-muted-foreground font-normal tracking-normal text-sm leading-5">
                     Get notified when the exchange rate reaches a certain
                     threshold. You can set the rate you want to be notified
                     about.
@@ -51,95 +52,97 @@ export const CreateRateAlert = ({
             </SheetTitle>
           </SheetHeader>
 
-          <div className="mx-auto w-full max-w-3xl py-3 px-4">
-            <div className="flex flex-col items-center space-y-4 w-full">
-              <CurrencySelect
-                selectedCurrency={''}
-                onCurrencySelect={() => {}} // eslint-disable-line
-                labelProps={{
-                  className: 'bg-zinc-50',
-                  children: 'From',
-                }}
-              />
+          <ScrollArea className="overflow-y-auto">
+            <div className="mx-auto w-full max-w-3xl py-8">
+              <div className="flex flex-col items-center space-y-4 w-full">
+                <CurrencySelect
+                  selectedCurrency={''}
+                  onCurrencySelect={() => {}} // eslint-disable-line
+                  labelProps={{
+                    className: 'bg-zinc-50',
+                    children: 'From',
+                  }}
+                />
 
-              <Button
-                variant="outline"
-                className="rounded-full size-10 p-0 flex-shrink-0"
-              >
-                <span className="sr-only">Click me</span>
-                <ArrowUpDown className="size-5" />
-              </Button>
+                <Button
+                  variant="outline"
+                  className="rounded-full size-10 p-0 flex-shrink-0"
+                >
+                  <span className="sr-only">Click me</span>
+                  <ArrowUpDown className="size-5" />
+                </Button>
 
-              <CurrencySelect
-                selectedCurrency={''}
-                onCurrencySelect={() => {}} // eslint-disable-line
-                labelProps={{
-                  className: 'bg-zinc-50',
-                  children: 'To',
-                }}
-              />
-            </div>
+                <CurrencySelect
+                  selectedCurrency={''}
+                  onCurrencySelect={() => {}} // eslint-disable-line
+                  labelProps={{
+                    className: 'bg-zinc-50',
+                    children: 'To',
+                  }}
+                />
+              </div>
 
-            <div className="flex flex-col space-y-6 mt-6">
-              <div>
-                <h5>Daily Updates</h5>
-                <div className="flex items-center justify-between space-x-4 mt-1">
-                  <Label
-                    htmlFor="daily-updates"
-                    className="font-normal text-muted-foreground tracking-normal leading-[24px]"
-                  >
-                    Get daily updates on the exchange rate between the
-                    currencies you selected.
-                  </Label>
-                  <Switch id="daily-updates" />
+              <div className="flex flex-col space-y-6 mt-6">
+                <div>
+                  <h5>Daily Updates</h5>
+                  <div className="flex items-center justify-between space-x-4 mt-1">
+                    <Label
+                      htmlFor="daily-updates"
+                      className="font-normal text-muted-foreground tracking-normal leading-5 text-sm"
+                    >
+                      Get daily updates on the exchange rate between the
+                      currencies you selected.
+                    </Label>
+                    <Switch id="daily-updates" />
+                  </div>
+                </div>
+                <div>
+                  <h5>Threshold Updates</h5>
+                  <div className="flex items-center justify-between space-x-4 mt-1">
+                    <Label
+                      htmlFor="threshold-updates"
+                      className="font-normal text-muted-foreground tracking-normal leading-5 text-sm"
+                    >
+                      Get notified when the exchange rate reaches a certain
+                      threshold.
+                    </Label>
+                    <Switch id="threshold-updates" />
+                  </div>
+                </div>
+                <div>
+                  <h5></h5>
                 </div>
               </div>
-              <div>
-                <h5>Threshold Updates</h5>
-                <div className="flex items-center justify-between space-x-4 mt-1">
-                  <Label
-                    htmlFor="threshold-updates"
-                    className="font-normal text-muted-foreground tracking-normal leading-[24px]"
-                  >
-                    Get notified when the exchange rate reaches a certain
-                    threshold.
-                  </Label>
-                  <Switch id="threshold-updates" />
-                </div>
-              </div>
-              <div>
-                <h5></h5>
-              </div>
-            </div>
 
-            <div className="mb-8">
-              <h5>Desired Rates</h5>
-              <div className="flex space-x-12 items-center mt-1">
-                <div className="flex-shrink-0">
-                  <div className="flex items-center">
-                    <CurrencyIcon
-                      currency={currencyFrom}
-                      className="mr-2 flex items-center"
+              <div className="mb-8">
+                <h5>Desired Rates</h5>
+                <div className="flex space-x-12 items-center mt-1">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center">
+                      <CurrencyIcon
+                        currency={currencyFrom}
+                        className="mr-2 flex items-center"
+                      />
+                      <h4>1 {currencyFrom} =</h4>
+                    </div>
+                  </div>
+                  <div className="relative flex-1">
+                    <Input
+                      type="text"
+                      pattern="^[0-9]*[.]{0,1}[0-9]*$"
+                      aria-label="rate-amount"
                     />
-                    <h4>1 {currencyFrom} =</h4>
-                  </div>
-                </div>
-                <div className="relative flex-1">
-                  <Input
-                    type="text"
-                    pattern="^[0-9]*[.]{0,1}[0-9]*$"
-                    aria-label="rate-amount"
-                  />
-                  <div className="absolute top-1/3 right-6 flex items-center">
-                    <CurrencyIcon currency={currencyTo} size="sm" />
-                    <span className="text-xs pl-2">{currencyTo}</span>
+                    <div className="absolute top-1/3 right-6 flex items-center">
+                      <CurrencyIcon currency={currencyTo} size="sm" />
+                      <span className="text-xs pl-2">{currencyTo}</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </ScrollArea>
 
-          <SheetFooter className="bg-zinc-50 border-t border-zinc-100">
+          <SheetFooter className="bg-zinc-50 border-t border-zinc-100 absolute bottom-0 w-full left-0">
             <div className="mx-auto w-full max-w-sm py-4 px-0">
               <div className="flex justify-between space-x-4">
                 <Button className="flex-1" size="lg">
