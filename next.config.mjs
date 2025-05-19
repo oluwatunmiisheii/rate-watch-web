ac5548ba7f3474822a777ae391a1584a6d8e9356
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-05-19 02:07:11 +0100

    chore: remove unused headers

--- next.config.mjs
+++ next.config.mjs
@@ -32,23 +32,6 @@ const nextConfig = {
           },
         ],
       },
-      {
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
     ]
   },
 }
